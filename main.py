8b3cedc911d8aadb3c590a32a5aae39c80b77a6a
Qingyao Yu <papercut1002@gmail.com>
2024-01-09 20:07:38 -0500

    Replace with new sample project

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from flask import Flask, jsonify, request, render_template, url_for,stream_with_context, Response
+from flask import Flask, jsonify, request, render_template, url_for,stream_with_context, Response, send_file, after_this_request
 from flask_cors import CORS
 import openai
 from openai import OpenAI
 import os
-import json
-
+import random
 app = Flask(__name__)
 CORS(app)
 openai.api_key = os.getenv("OPENAI_API_KEY")  # Api key
-# Define initial states
-
-
 initial_conversation = [{
     "role": "system",
     "content": ("You are a helpful assistant for answering rail-related questions. You should provide specific explanations and extend the related knowledge to the user. However, you also need to keep the response concise.")
@@ -26,6 +22,14 @@ initial_explain_list = [{
     "content": "You are a helpful assistant, skilled in explaining rail-related concepts. "
                 "Your responses should be clear and educational but need to be brief and concise."
                 
+}]
+initial_determine_list = [{
+    "role": "system",
+    "content": ("You are a highly intelligent assistant capable of understanding complex requests related to image generation. "
+                "When you receive a user message, your task is to analyze it and determine the nature of the request. "
+                "If the user's message implies a request for an image, respond with 'type: image' followed by a precise and relevant keyword or phrase that can be used to generate an image with a model like DALL-E. "
+                "For example, if the user asks for a picture of a 'sunset over mountains', you should respond with 'type: image' and 'keyword: sunset over mountains'. "
+                "If the user's message does not imply a request for an image, respond with 'type: text'.Your respond should not contain other text since the app is going to use different function by matching depends on your respond")
 }]
 
 # Global variables to maintain state
@@ -85,9 +89,10 @@ def get_gpt3_question():
                 '[{"question": "What is the fastest train in the world?", "answers": [{"text": "Shinkansen", "correct": false}, {"text": "TGV", "correct": false}, {"text": "Maglev", "correct": true}, {"text": "Eurostar", "correct": false}]},'
                 '{"question": "Which city has the oldest metro system?", "answers": [{"text": "New York", "correct": false}, {"text": "Paris", "correct": false}, {"text": "London", "correct": true}, {"text": "Tokyo", "correct": false}]}]'
 }]
-    response_data = gpt4_dialogue(message)  # Call GPT-4 with the full message
+    response_data = gpt4_dialogue(message)  
     return jsonify(response_data.strip('```json'))
 
+#Default endpoint
 @app.route('/stream', methods=['POST'])
 def stream():
     user_input = request.json['message']
@@ -109,20 +114,18 @@ def stream():
         conversation.append({"role": "assistant", "content": full_response})
     return Response(stream_with_context(generate()), mimetype='text/event-stream')
 
+#hint endpoint(before answer question)
 @app.route('/hint', methods=['POST'])
 def hint():
-    user_input = request.json['message']  # 用户在类型容器中输入的文本
-    current_question = request.json['currentQuestion']  # 当前问题文本
-    options = request.json['options']  # 当前问题的选项列表
-
-    # 构建对话历史，包括当前问题、选项和用户输入
+    user_input = request.json['message']  
+    current_question = request.json['currentQuestion']  
+    options = request.json['options'] 
     hint_list.append({"role": "system", "content": f"Current question: {current_question}"})
     options_content = ", ".join([f"{opt['text']}" for opt in options])
     hint_list.append({"role": "system", "content": f"Options: {options_content}"})
     hint_list.append({"role": "user", "content": user_input})
 
     def generate():
-        # 请求 GPT-4 获取提示
         try:
             stream = client.chat.completions.create(
                 model="gpt-4-1106-preview",
@@ -140,28 +143,22 @@ def hint():
 
     return Response(stream_with_context(generate()), mimetype='text/event-stream')
 
+#explain endpoint(after answer question)
 @app.route('/explain', methods=['POST'])
 def explain():
     current_question = request.json['currentQuestion']
     selected_option = request.json['selectedOption']
     is_correct = request.json['isCorrect']
     options = request.json['options']
-
-    # 构建对话历史，包括问题、选项和用户选择
     options_content = ", ".join([f"{opt['text']}" for opt in options])
     explain_list.append({"role": "system", "content": f"Question: {current_question}, Options: {options_content}, Selected: {selected_option}"})
-
-    # 根据用户选择的正确性更新系统提示
     if is_correct:
         system_prompt = "The user has selected the correct answer. Provide encouragement and extend the topic with additional related information."
     else:
         system_prompt = "The user has selected the wrong answer. Provide a clear explanation for the correct answer and extend the topic with additional related information."
-
-    # 添加系统提示到对话历史
     explain_list.append({"role": "system", "content": system_prompt})
    
     def generate():
-        # Stream Prompt
         try:
             stream = client.chat.completions.create(
                 model="gpt-4-1106-preview",
@@ -179,6 +176,122 @@ def explain():
 
     return Response(stream_with_context(generate()), mimetype='text/event-stream')
 
+
+voices = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]#supported type of voice
+@app.route('/tts', methods=['POST'])
+def text_to_speech():
+    data = request.get_json()
+    text = data['text']
+    voice = random.choice(voices)
+    try:
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice=voice,
+            input=text,
+            speed=1.2
+        )
+
+        audio_file_path = os.path.join(app.root_path, 'speech.mp3')
+        response.stream_to_file(audio_file_path)
+
+        return send_file(audio_file_path, mimetype='audio/mpeg')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({"error": "TTS generation failed"}), 500
+
+#image-generation endpoint
+@app.route('/generate-image', methods=['POST'])
+def generate_image():
+    data = request.get_json()
+    prompt = data['prompt']
+    image_url = create_image(prompt)
+    return jsonify({'imageUrl': image_url})
+
+def create_image(prompt):
+    try:
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=f"A photograph of {prompt}",
+            size="1792x1024",
+            quality="standard",
+            n=1,
+)
+        url = response.data[0].url
+        print(url)
+        return url
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({"error": "Image generation failed"}), 500
+    
+import googleapiclient.discovery
+
+api_key = 'AIzaSyD3i_ko5dJtPDu6IrYF2mCWtgMA024O1e0'
+#google api client youtube endpoint
+def search_youtube_videos(api_key, search_query):
+    youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
+
+    search_response = youtube.search().list(
+        q=search_query,
+        part='snippet',
+        maxResults=1
+    ).execute()
+
+    videos = []
+    for video in search_response.get('items', []):
+        if video['id']['kind'] == 'youtube#video':
+            video_title = video['snippet']['title']
+            video_id = video['id']['videoId']
+            video_url = f"https://www.youtube.com/watch?v={video_id}"
+            videos.append((video_title, video_url))
+
+    return videos
+#youtube video search endpoint
+@app.route('/search-video', methods=['POST'])
+def search_video():
+    data = request.get_json()
+    search_query = data.get('query', '')
+    video_results = search_youtube_videos(api_key, search_query)
+    for video_title, video_url in video_results:
+        print(f'Title: {video_title}')
+        print(f'URL: {video_url}')
+    if video_results:
+        video_title, video_url = video_results[0]
+    else:
+        video_title, video_url = ("No video found", "")
+    return jsonify({'videoTitle': video_title, 'videoUrl': video_url})
+
+
+@app.route('/gpt-determine-action', methods=['POST'])
+def gpt_determine_action():
+    data = request.get_json()
+    message = data['message']
+    initial_determine_list = [{
+    "role": "system",
+    "content": ("You are a highly intelligent assistant capable of understanding complex requests related to image generation. "
+                "When you receive a user message, your task is to analyze it and determine the nature of the request. "
+                "If the user's message implies a request for an image, respond with 'type: image' followed by a precise and relevant keyword or phrase that can be used to generate an image with a model like DALL-E. "
+                "For example, if the user asks for a picture of a 'sunset over mountains', you should respond with 'type: image' and 'keyword: sunset over mountains'. "
+                "If the user's message does not imply a request for an image, respond with 'type: text'.Your respond should not contain other text since the app is going to use different function by matching depends on your respond")
+}]
+    initial_determine_list.append({"role": "user", "content": message})
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=initial_determine_list,
+        )
+        response_content = response.choices[0].message.content
+        if response_content.startswith('type: image'):
+            keyword = response_content.split('keyword: ')[1] if 'keyword: ' in response_content else ""
+            return jsonify({'type': 'image', 'keyword': keyword})
+        else:
+            return jsonify({'type': 'text'})
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Error processing the GPT-4 request."}), 500
+
+
+
+
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000, debug=True)
     app.run(host='0.0.0.0', port=4000)
+    
--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
+let questions = [];
+
+// When the page loads, set up event listeners and start the quiz.
 document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.getElementById('start-btn');
     const startScreen = document.getElementById('start-screen');
@@ -5,16 +8,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Event handler for clicking the start button
     startButton.addEventListener('click', () => {
-        startScreen.style.display = 'none'; // Hide the start screen
-        quizApp.style.display = 'block'; // Show the quiz interface
-        fetchQuizQuestions(); // Fetch questions
+        const topic = document.querySelector('input.topic-input').value.trim() || 'rail and transportation';
+        startScreen.style.display = 'none'; 
+        quizApp.style.display = 'flex'; 
+        fetchQuizQuestions(topic); 
+    });
+
+    // Add event listener for confirm button
+    let confirmButton = document.getElementById('confirm-btn');
+    if (confirmButton) {
+        confirmButton.addEventListener('click', confirmAnswer);
+    }
+
+    // Add event listener for Enter key in textarea
+    const textarea = document.querySelector('.typing-textarea textarea');
+    textarea.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+            sendMessage();
+        }
     });
 });
 
 
-function fetchQuizQuestions() {
+// Function to fetch quiz questions based on the provided topic.
+function fetchQuizQuestions(topic) {
     showTypingAnimation();
-    fetch('https://12431dc6f53af306.ngrok.app/ask')
+    fetch('/ask', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ topic: topic }), // Send the topic as JSON
+    })
         .then(response => response.json())
         .then(data => {
             removeTypingAnimation();
@@ -37,11 +63,229 @@ function fetchQuizQuestions() {
         });
 }
 
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
+// Function to handle user input and initiate actions.
+function sendMessage() {
+    const textarea = document.querySelector('.typing-textarea textarea');
+    const message = textarea.value.trim();
+    textarea.value = '';
 
+    if (message) {
+        displayMessage('user', message);
+        // First, call the '/gpt-determine-action' endpoint
+        fetch('/gpt-determine-action', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message: message })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(data.type === 'image') {
+                // If the type is image, call the 'handleImageGenerationCommand' function
+                handleImageGenerationCommand(data.keyword);
+            } else {
+                // If the type is text, determine the next step based on the 'answerConfirmed' status
+                let endpoint = answerConfirmed ? '/stream' : '/hint';
+                fetch(endpoint, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ message: message, currentQuestion: currentQuestion, options: currentOptions })
+                })
+                .then(response => response.body.getReader())
+                .then(reader => readStream(reader));
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    }
+}
 
+// Function to handle image generation command and display generated images.
+function handleImageGenerationCommand(prompt) {
+    updateOrDisplayAssistantMessage("Here is a photo of " + prompt);
+    showImageLoader();
+    fetch('/generate-image', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: prompt }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.imageUrl) {
+            // Remove the loader animation
+            removeImageLoader();
+            displayGeneratedImage(data.imageUrl); // Display the image
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Function to display generated images in the chat.
+function displayGeneratedImage(imageUrl) {
+    // Get the chat container
+    const chatContainer = document.querySelector('.chat-container');
+    
+    // Create a new chat message element
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('message');
+
+    // Create an image element and set its source URL and CSS class
+    const imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+    imgElement.alt = 'Generated content';
+    imgElement.classList.add('chat-image');  // Apply CSS class
+
+    // Append the image element to the message element
+    messageDiv.appendChild(imgElement);
+    
+    // Append the message element to the chat container
+    chatContainer.appendChild(messageDiv);
+    
+    // Scroll the chat container to the bottom to show the new message
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+let accumulatedText = ''; // Variable to accumulate received text blocks
+
+async function readStream(reader, source) {
+    const decoder = new TextDecoder();
+    let isFirstChunk = true;
+    let completeMessage = '';
+    try {
+        while (true) {
+            const { value, done } = await reader.read();
+            if (done) {
+                break; 
+            }
+            let chunk = decoder.decode(value, { stream: true });
+            completeMessage += chunk;
+           updateOrDisplayAssistantMessage(chunk);
+        }
+    } catch (error) {
+        console.error('Error reading the stream:', error);
+    }
+    await playTextAsSpeech(completeMessage);
+    completeMessage = ''; // reset accumlated message
+    isFirstChunk = true;
+    if (source === 'explain') {
+        searchAndDisplayVideo(currentQuestion);//recommend youtube video based on current question
+        nextButton.style.display = "block";
+    }
+}
+
+function searchAndDisplayVideo(query) {
+    fetch('/search-video', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: query }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        displayYouTubeVideo(data.videoTitle, data.videoUrl);
+    })
+    .catch(error => console.error('Error:', error));
+}
+function updateOrDisplayAssistantMessage(chunk) {
+    const chatContainer = document.querySelector('.chat-container');
+    let lastMessage = chatContainer.querySelector('.assistant-message:last-child .message-content');
+    
+    if (lastMessage) {
+        lastMessage.textContent += chunk; // Append new text chunk to the last assistant message
+    } else {
+        displayMessage('assistant', chunk); // There is no last assistant message, display a new one
+    }
+    
+    // Scroll to the bottom of the chat container
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+function displayMessage(sender, message) {
+  const chatContainer = document.querySelector('.chat-container');
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add('message', sender + '-message');
+
+  const avatarLabelContainer = document.createElement('div');
+  avatarLabelContainer.classList.add('avatar-label-container');
+
+  const avatarDiv = document.createElement('div');
+  avatarDiv.classList.add('avatar');
+  const avatarImg = document.createElement('img');
+  avatarImg.src = sender === 'user' ? '/static/images/user.png' : '/static/images/assistant.png';
+  avatarDiv.appendChild(avatarImg);
+
+  const labelDiv = document.createElement('div');
+  labelDiv.classList.add('label');
+  labelDiv.textContent = sender === 'user' ? 'You' : 'ChatGPT';
+
+  avatarLabelContainer.appendChild(avatarDiv);
+  avatarLabelContainer.appendChild(labelDiv);
+
+  const messageContentDiv = document.createElement('div');
+  messageContentDiv.classList.add('message-content');
+  messageContentDiv.textContent = message;
+
+  messageDiv.appendChild(avatarLabelContainer);
+  messageDiv.appendChild(messageContentDiv);
+
+  chatContainer.appendChild(messageDiv);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+function displayYouTubeVideo(title, url) {
+    if (!url) return;
+
+    const chatContainer = document.querySelector('.chat-container');
+    const videoDiv = document.createElement('div');
+    videoDiv.classList.add('youtube-video');
+    const description = `I found this video titled "${title}" that might help you learn more about the topic.`;
+    playTextAsSpeech(description);
+    const videoId = url.split('watch?v=')[1];
+    const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+
+    const videoIframe = document.createElement('iframe');
+    videoIframe.src = embedUrl;
+    videoIframe.title = title;
+    videoIframe.frameBorder = "0";
+    videoIframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    videoIframe.allowFullscreen = true;
+    videoIframe.style.width = '50%'; 
+    videoIframe.style.height = '40%'; 
+
+    videoDiv.appendChild(videoIframe);
+    chatContainer.appendChild(videoDiv);
+    updateOrDisplayAssistantMessage(description);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+function playTextAsSpeech(text) {
+    return new Promise((resolve, reject) => {
+        fetch('/tts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: text })
+        })
+        .then(response => response.blob())
+        .then(blob => {
+            const url = URL.createObjectURL(blob);
+            const audio = new Audio(url);
+            audio.addEventListener('ended', () => {
+                URL.revokeObjectURL(url); // Clean up the URL
+                resolve(); // Resolve the promise when audio playback ends
+            });
+            audio.play();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            reject(error); 
+        });
+    });
+}
 let currentQuestionIndex = 0;
 let score = 0;
 
@@ -54,6 +298,8 @@ function startQuiz(){
 
 function showQuestion(){
     resetState();
+    answerConfirmed = false;
+    fetchQuestionData();
     let currentQuestion = questions[currentQuestionIndex];
 
     let questionNo = currentQuestionIndex + 1;
@@ -83,31 +329,129 @@ function removeTypingAnimation() {
     const animationContainer = document.getElementById('animation-container');
     animationContainer.innerHTML = '';
 }
+function showImageLoader() {
+    const chatContainer = document.querySelector('.chat-container');
+    const loaderHtml = `<div class="image-loader"></div>`;
+    chatContainer.insertAdjacentHTML('beforeend', loaderHtml);
+}
+
+function removeImageLoader() {
+    const imageLoader = document.querySelector('.image-loader');
+    if (imageLoader) {
+        imageLoader.remove();
+    }
+}
+
 
 function resetState(){
+    showConfirmButton();
     nextButton.style.display = "none";
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
-function selectAnswer(e){
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect){
-        selectedBtn.classList.add("correct");
-        score++;
-    }else{
-        selectedBtn.classList.add("incorrect");
+let selectedAnswerButton = null;
+
+function selectAnswer(e) {
+    console.log("Clicked button:", e.target);
+    if (selectedAnswerButton) {
+        selectedAnswerButton.classList.remove("selected");
     }
+    selectedAnswerButton = e.target;
+    selectedAnswerButton.classList.add("selected");
+    const confirmButton = document.getElementById('confirm-btn');
+    confirmButton.removeAttribute('disabled');
+    confirmButton.classList.add('green-btn');
+}
+
+
+function showToast(message) {
+    const toast = document.createElement('div');
+    toast.textContent = message;
+    toast.className = 'toast-message'; 
+    document.body.appendChild(toast);
+    setTimeout(() => {
+        toast.classList.add('show'); 
+        setTimeout(() => {
+            toast.classList.remove('show'); 
+            setTimeout(() => document.body.removeChild(toast), 500); 
+        }, 3000); 
+    }, 100); 
+}
+
+function confirmAnswer() {
+    answerConfirmed = true;
+    
+    if (!selectedAnswerButton) {
+        console.error('No answer selected');
+        showToast("Please select an answer before confirming.");
+        return;
+    }
+    selectedAnswerButton.classList.remove("selected");
+    const isCorrect = selectedAnswerButton.dataset.correct === "true";
+    if (isCorrect) {
+        console.log("correct")
+        const correctSound = document.getElementById('correct-sound');
+        correctSound.play();
+        selectedAnswerButton.classList.add("correct");
+        score++;
+    } else {
+        const incorrectSound = document.getElementById('incorrect-sound');
+        incorrectSound.play();
+        selectedAnswerButton.classList.add("incorrect");
+    }
+
     Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === "true"){
+        button.disabled = true;
+        if (button.dataset.correct === "true") {
             button.classList.add("correct");
         }
-        button.disabled = true;
     });
-    nextButton.style.display = "block";
+
+    document.getElementById('confirm-btn').style.display = 'none'; 
+    displayMessage('user', selectedAnswerButton.textContent);
+  
+    let postData = {
+        currentQuestion: currentQuestion,
+        selectedOption: selectedAnswerButton.textContent,
+        isCorrect: isCorrect,
+        options: currentOptions
+    };
+    showToast("Feedback generating ...");
+    
+    fetch('/explain', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+    })
+    .then(response => {
+        const reader = response.body.getReader();
+        readStream(reader, 'explain');
+    })
+    .catch(error => console.error('Error:', error));
 }
+
+
+
+
+function showConfirmButton() {
+    let confirmButton = document.getElementById('confirm-btn');
+    if (!confirmButton) {
+        confirmButton = document.createElement('button');
+        confirmButton.id = 'confirm-btn';
+        confirmButton.textContent = 'Confirm';
+        confirmButton.classList.add('btn');
+        document.querySelector('.quiz-container').appendChild(confirmButton);
+    }
+    confirmButton.style.display = 'block';
+    confirmButton.disabled = true;
+    console.log('Confirm button should be visible now');
+    confirmButton.addEventListener('click', confirmAnswer);
+}
+
 
 function showScore() {
     resetState();
@@ -134,9 +478,8 @@ function showScore() {
 
     // Add the back button to the interface
     answerButtons.appendChild(backButton);
-
-    nextButton.innerHTML = "Play Again";
-    nextButton.style.display = "block";
+    //nextButton.innerHTML = "Play Again";
+    //nextButton.style.display = "block";
 }
 
 function handleNextButton(){
@@ -156,8 +499,5 @@ nextButton.addEventListener("click", ()=>{
         startQuiz();
     }
 });
-
-
-
 
 startQuiz();
